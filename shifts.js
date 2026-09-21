91f43359f5cd57e96031ac7c5a9f557891cb5c4e
Derk Reyes <derkreyes65@gmail.com>
2026-09-21 19:43:03 +0800

    Round shift pay calculation - reyes.derk

--- shifts.js
+++ shifts.js
@@ -3,11 +3,7 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
-  if (hours > 8) {
-    return Math.floor((8 * rate) + ((hours - 8) * rate * 1.5));
-  }
-
-  return Math.floor(hours * rate);
+  return Math.round(hours * rate);
 }
 
 module.exports = { isValidShift, calculatePay };
