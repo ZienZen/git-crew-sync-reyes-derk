83e0d66abec1bc074143c96b4b6e8eb8dc50907d
Derk Reyes <derkreyes65@gmail.com>
2026-09-21 20:13:51 +0800

    Adjust pay rounding in overtime calculation - reyes.derk

--- shifts.js
+++ shifts.js
@@ -3,7 +3,11 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
-  return Math.round(hours * rate);
+  if (hours > 8) {
+    return Math.ceil((8 * rate) + ((hours - 8) * rate * 1.5));
+  }
+
+  return Math.ceil(hours * rate);
 }
 
 module.exports = { isValidShift, calculatePay };
